4108ff76ec967db64068f227e1f44efff4bdb4d8
gustavocassioli <88606199+gustavocassioli@users.noreply.github.com>
2022-01-31 00:42:49 -0300

    first file shipment

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Hides the body scrollbar when the overlay menu is opened */
+
+// Get the body element
+const body = document.body;
+
+// Get the checkbox element
+const hamburgerToggler = document.querySelector(".toggler");
+
+// Set event listener to the hamburger toggler
+// so that we can detect when the overlay is opened and hid
